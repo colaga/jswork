0b5f41fdecda923f92ea80b82df9542764931026
colaga <3151585876@qq.com>
2019-10-14 15:44:09 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,8 +1,8 @@
 //9X9的空表
 str = '<table border="1">'
-for(let i=1;i<10;--i){
+for(let i=1;i<10;++i){
     str += '<tr>'
-    for(var j=1;j<10;--j){
+    for(var j=1;j<10;++j){
         str += '<td>&nbsp;</td>'
     }
 }
@@ -22,12 +22,13 @@ str += '</table>'
 document.getElementById('table2').innerHTML = str
 
 //倒九九乘法表
-str = '<table border="1>'
-for(let i =1;i<10;--i){
+str = '<table border="1">'
+for(var i =9;i>0;--i){
     str += '<tr>'
-    for(var j=1;j>1;--j){
+    for(var j=1;j>0;--j){
         str += '<td>' + i + '*' + j + '=' + ( i * j ) +'</td>'
     }
+    str += '</tr>'
 }
 str += '</table>'
 document.getElementById('table3').innerHTML = str
