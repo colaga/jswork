453db90c4cbd9ec8b3fe5b9dff914d2d86bd1d88
colaga <3151585876@qq.com>
2019-10-15 15:35:55 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,21 @@
+function calculate(){
+    let num =document.getElementById('colaga').nodeValu
+    num = parseFloat(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('colaga')
+    }
+}
+function factorial(n){
+    let sum =1
+    for(let i=1)
+
+}
+return sum
+function oddFactorisl(n){
+    let sum = 1
+    for(let i=1)
+}
+return sum
+function pi(num){
+    
+} 
