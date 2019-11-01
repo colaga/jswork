71621ad5e132e4ff10715644dd409d56513a50e5
colaga <3151585876@qq.com>
2019-11-01 21:57:33 +0800

    work10

--- work10/index.js
+++ work10/index.js
@@ -1,23 +1,35 @@
 function monkey(){
-    let total = document.getElementById("monkeytotal").nodeValue
-    let kick =document.getElementById("monkeykick").nodeValue
+    let total = document.getElementById("monkeytotal").value
+    let kick = document.getElementById("monkeykick").value
     total = parseInt(total) && Number(total)
-    kick = parseInt(kick) &&Number(kick)
-    if(isNaN(total)||isNaN(kick)){
+    kick = parseInt(kick) && Number(kick)
+    if(isNaN(total) || isNaN(kick)){
         alert('请输入数字')
         return
     }
-    let monkey =[]
-    for(let i = 1;i<=total;i++){
-        monkey.puah(i)
+    let monkey=[]
+    for(let i=1;i<=total;i++){
+        monkey.push(i)
     }
-    let i = 0
-    while(monkey.length>1){
+    let i=0
+    while (monkey.length>1){
         i++;
         head = monkey.shift()
         if(i%kick!=0){
-        monkey.push(head);
+            monkey.push(head);
+        }
     }
-   }
-    document.getElementById('monkeyking').innerText +monkey[0]
+    document.getElementById('monkeyking').innerText = monkey[0]
+}
+function stat(){
+    let str = document.getElementById("str").value
+    let obj = {}
+    let shuzu  = str.split("")
+    
+    shuzu = shuzu.sort()
+    for (let i = 0; i < str.length; ++i) {
+        let moudu = shuzu[i]
+        obj[moudu] = (obj [moudu] + 1) || 1;
+    }
+    document.getElementById("result").innerText = JSON.stringify(obj)
 }
